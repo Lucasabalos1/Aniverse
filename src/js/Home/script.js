0528463d168e67a34ad473364da73c38b8c3abc4
unknown <lukotas100@gmail.com>
2024-07-24 03:38:40 -0300

    add styles in random section, missing logic

--- src/js/Home/script.js
+++ src/js/Home/script.js
@@ -8,6 +8,7 @@ const swiperPopularWrapper = document.getElementById("swiper-popular-wrapper");
 const toggleMenu = () =>{
     modal.classList.toggle("visible-modal");
     lateralMenu.classList.toggle("visible-menu");
+    document.body.style.overflow = (modal.classList.contains("visible-modal") ? "hidden" : "scroll")
 }
 
 const showMessageEmptyList = () => {
