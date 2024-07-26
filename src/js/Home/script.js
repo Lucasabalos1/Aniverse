6bda34ddce55a29fbec5a2164bf38bf9938ec53f
unknown <lukotas100@gmail.com>
2024-07-26 00:28:00 -0300

    Extracted form and logic from a previous project and adapted it for this project

--- src/js/Home/script.js
+++ src/js/Home/script.js
@@ -9,6 +9,7 @@ const closeBtn = document.getElementById("close-modal-button");
 const swiperWelcomeWrapper = document.getElementById("swiper-welcome-wrapper");
 const swiperPopularWrapper = document.getElementById("swiper-popular-wrapper");
 const randomSection = document.getElementById("recommendation-random-section");
+const formBtn = document.getElementById("send-button");
 
 const toggleMenu = () =>{
     modal.classList.toggle("visible-modal");
@@ -219,6 +220,38 @@ const generateAnimeRandom = async () => {
         generateAnimeRandom();
     }
 };
+
+const verifyMail = () => {
+    const email = document.getElementById("email_input").value;
+    //El metodo some devuelve true si al menos un elemento cumple la condicion que se pide en la funcion
+    return ["@gmail.com", "@hotmail.com", "@outlook"].some(domain => email.includes(domain));
+}
+
+
+document.getElementById("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (!verifyMail()) {
+        alert("Por favor, ingrese un correo electrónico válido con uno de los siguientes dominios: @gmail.com/.ar, @outlook.com/.ar, @hotmail.com/.ar");
+        return;
+    }
+    
+    formBtn.value = "ENVIANDO EMAIL...";
+
+    const serviceID = 'default_service';
+    const templateID = 'template_98e401c';
+
+    emailjs.sendForm(serviceID, templateID, document.getElementById("form"))
+    .then(() => {
+        formBtn.value = 'ENVIAR EMAIL';
+        console.log('Correo enviado exitosamente!');
+        alert('Email mandado!');
+      }, (err) => {
+        formBtn.value = 'ENVIAR EMAIL';
+        console.log('Correo no enviado!');
+        alert(JSON.stringify(err));
+      });
+})
 
 modalBtn.addEventListener("click", toggleMenu);
 closeBtn.addEventListener("click", toggleMenu);
