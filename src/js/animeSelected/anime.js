9253cdb1a10c83c18d305f35e47be30059f301ba
unknown <lukotas100@gmail.com>
2024-07-31 23:02:46 -0300

    add files from anime selected section

---
+++ src/js/animeSelected/anime.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById("modal-menu-container");
+const lateralMenu = document.querySelector(".menu-lateral-container");
+const modalBtn = document.querySelector(".modal-button");
+const closeBtn = document.getElementById("close-modal-button");
+
+
+
+const toggleMenu = () =>{
+    modal.classList.toggle("visible-modal");
+    lateralMenu.classList.toggle("visible-menu");
+    document.body.style.overflow = (modal.classList.contains("visible-modal") ? "hidden" : "scroll")
+}
+
+modalBtn.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
